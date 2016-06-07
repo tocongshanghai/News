5b19f0a9fb994e94efbb29470387eccf143ef019
tocongshanghai <tocongshanghai@gmail.com>
2016-06-07 17:18:34 +0800

    第一个splash页面

---
+++ NewsApplication/smartnews/src/main/java/com/tocong/smartnews/SplashActivity.java
@@ -0,0 +1,80 @@
+package com.tocong.smartnews;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.RotateAnimation;
+import android.view.animation.ScaleAnimation;
+import android.widget.RelativeLayout;
+
+public class SplashActivity extends Activity {
+private RelativeLayout rlRoot;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        rlRoot= (RelativeLayout) findViewById(R.id.rl_root);
+        startAnimation();
+
+    }
+
+    private void startAnimation(){
+        AnimationSet animationSet=new AnimationSet(false);
+
+        /*
+        * 旋转动画
+        * */
+        RotateAnimation rotateAnimation=new RotateAnimation(0,360, Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f);
+        rotateAnimation.setDuration(3000);
+        rotateAnimation.setFillAfter(true);
+
+
+        /*
+        * 缩放动画
+        * */
+        ScaleAnimation scaleAnimation=new ScaleAnimation(0,1,0,1,Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f);
+        scaleAnimation.setDuration(3000);
+        scaleAnimation.setFillAfter(true);
+
+
+        /*
+        * 渐变动画
+        * */
+        AlphaAnimation alphaAnimation=new AlphaAnimation(0,1);
+        alphaAnimation.setDuration(3000);
+        alphaAnimation.setFillAfter(true);
+
+
+        animationSet.addAnimation(rotateAnimation);
+        animationSet.addAnimation(scaleAnimation);
+        animationSet.addAnimation(alphaAnimation);
+        animationSet.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                jumpNextPage();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        rlRoot.startAnimation(animationSet);
+    }
+
+    private void jumpNextPage(){
+
+        startActivity(new Intent(SplashActivity.this,MainActivity.class));
+        finish();
+
+
+    }
+}
